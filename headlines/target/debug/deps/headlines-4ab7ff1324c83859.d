ecb3e1511587fe379f5437cb9bf26b52318397a6
Gotam Gorabh <gautamy672@gmail.com>
2022-10-20 17:12:24 +0530

    builded main.rs

--- headlines/target/debug/deps/headlines-4ab7ff1324c83859.d
+++
@@ -1,5 +0,0 @@
-/home/gotamg/Headlines/headlines/target/debug/deps/headlines-4ab7ff1324c83859: src/main.rs
-
-/home/gotamg/Headlines/headlines/target/debug/deps/headlines-4ab7ff1324c83859.d: src/main.rs
-
-src/main.rs:
--- headlines/target/debug/headlines.d
+++
@@ -1,1 +0,0 @@
-/home/gotamg/Headlines/headlines/target/debug/headlines: /home/gotamg/Headlines/headlines/src/main.rs
